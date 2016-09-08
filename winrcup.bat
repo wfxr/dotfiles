f64f9b4adf6e57b23a206f1519706a8ac4c97978
Wenxuan Zhang <wenxuan-zhang@outlook.com>
2016-09-08 21:04:31 +0800

    update

--- winrcup.bat
+++ winrcup.bat
@@ -1,2 +1,6 @@
 ::"%~dp0" standard for current dir(with postfix '\' included)
-mklink %USERPROFILE%\_vimrc %~dp0vimrc
+mklink %USERPROFILE%\_vimrc 		%~dp0vimrc
+mklink %USERPROFILE%\.ideavimrc 	%~dp0ideavimrc
+mklink %USERPROFILE%\.vimrc.commons	%~dp0vimrc.commons
+mklink %USERPROFILE%\.vimrc.plugins 	%~dp0vimrc.plugins
+mklink %USERPROFILE%\.vsvimrc 		%~dp0vsvimrc
