36af719a9a49123319c41a3dd205848d0d56dfb9
wfxr <wfxrgm@gmail.com>
2016-09-16 18:39:39 +0800

    format

--- winrcup.bat
+++ winrcup.bat
@@ -1,8 +1,8 @@
 ::"%~dp0" standard for current dir(with postfix '\' included)
-mklink %USERPROFILE%\.vimrc 		%~dp0vimrc
-mklink %USERPROFILE%\.ideavimrc 	%~dp0ideavimrc
-mklink %USERPROFILE%\.vimrc.commons	%~dp0vimrc.commons
+mklink %USERPROFILE%\.vimrc 		    %~dp0vimrc
+mklink %USERPROFILE%\.ideavimrc 	    %~dp0ideavimrc
+mklink %USERPROFILE%\.vimrc.commons	    %~dp0vimrc.commons
 mklink %USERPROFILE%\.vimrc.plugins 	%~dp0vimrc.plugins
-mklink %USERPROFILE%\.vimrc.gui 	%~dp0vimrc.gui
-mklink %USERPROFILE%\.vsvimrc 		%~dp0vsvimrc
+mklink %USERPROFILE%\.vimrc.gui 	    %~dp0vimrc.gui
+mklink %USERPROFILE%\.vsvimrc 		    %~dp0vsvimrc
 mklink %USERPROFILE%\.ycm_extra_conf.py %~dp0ycm_extra_conf.py
