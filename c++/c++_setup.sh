28b83c505add0b7d66882feea5177775ae96b503
Wenxuan <wenxuan-zhang@outlook.com>
2017-03-01 21:41:10 +0800

    add more config scripts
    Former-commit-id: f0b721c729dfe8b675e164451867690524b373ec

---
+++ c++/c++_setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# get the dir of the current script
+script_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+if [[ -a ~/.ycm_extra_conf.py ]]; then
+  mv ~/.ycm_extra_conf.py  ~/.ycm_extra_conf.py.bak
+fi
+
+if [[ -a ~/.clang-format ]]; then
+  mv ~/.clang-format ~/.clang-format.bak
+fi
+
+ln -s $script_dir/ycm_extra_conf.py ~/.ycm_extra_conf.py
+ln -s $script_dir/clang-format ~/.clang-format
+
--- setup_dotfiles.sh
+++ setup_dotfiles.sh
@@ -6,13 +6,11 @@ script_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 # -p says to create the dir if it doesn't exist already.
 mkdir -p ~/.config
 
-if [[ -a ~/.zshrc ]]
-then
+if [[ -a ~/.zshrc ]]; then
   mv ~/.zshrc ~/.zshrc.bak
 fi
 
-if [[ -a ~/.zshrc_theme_extra ]]
-then
+if [[ -a ~/.zshrc_theme_extra ]]; then
   mv ~/.zshrc_theme_extra ~/.zshrc_theme_extra.bak
 fi
 
@@ -22,29 +20,24 @@ ln -s $script_dir/zsh/zshrc_theme_extra ~/.zshrc_theme_extra
 # zsh - antigen
 mkdir -p ~/.antigen
 
-if [[ ! -a ~/.antigen/antigen ]]
-then
+if [[ ! -a ~/.antigen/antigen ]]; then
   git clone https://github.com/zsh-users/antigen.git ~/.antigen/antigen
 fi
 
 # vim
-if [[ -a ~/.vimrc ]]
-then
+if [[ -a ~/.vimrc ]]; then
   mv ~/.vimrc ~/.vimrc.bak
 fi
 
-if [[ -a ~/.vimrc_gui ]]
-then
+if [[ -a ~/.vimrc_gui ]]; then
   mv ~/.vimrc_gui ~/.vimrc_gui.bak
 fi
 
-if [[ -a ~/.vimrc_common ]]
-then
+if [[ -a ~/.vimrc_common ]]; then
   mv ~/.vimrc_common ~/.vimrc_common.bak
 fi
 
-if [[ -a ~/.vimrc_plugin ]]
-then
+if [[ -a ~/.vimrc_plugin ]]; then
   mv ~/.vimrc_plugin ~/.vimrc_plugin.bak
 fi
 
@@ -57,18 +50,31 @@ ln -s $script_dir/vim/vimrc_plugin ~/.vimrc_plugin
 ln -sf ~/.vim ~/.config/nvim
 ln -sf ~/.vimrc ~/.config/nvim/init.vim
 
-# tmux
-if [[ -a ~/.tmux ]]
-then
-  mv ~/.tmux ~/.tmux.bak
+# emacs
+if [[ -a ~/.spacemacs ]]; then
+    mv ~/.spacemacs ~/.spacemacs.bak
+fi
+ln -s $script_dir/emacs/spacemacs ~/.spacemacs
+
+if [[ ! -a ~/.emacs.d || ! -a ~/.emacs.d/spacemacs.mk ]]; then
+    git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
 fi
 
-if [[ -a ~/.tmux.conf ]]
-then
+# tmux
+if [[ -a ~/.tmux.conf ]]; then
   mv ~/.tmux.conf ~/.tmux.conf.bak
 fi
 
 ln -s $script_dir/tmux/tmux.conf ~/.tmux.conf
 
+# tmux - tpm
+if [[ ! -a ~/.tmux/plugins/tpm ]]; then
+  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+fi
+
 # bin
 mkdir -p $HOME/bin
+
+$script_dir/git/git_setup.sh
+$script_dir/clojure/clojure_setup.sh
+$script_dir/c++/c++_setup.sh
