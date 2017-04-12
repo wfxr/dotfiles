d4c8d765b61ab6440954991bb88f1570e122fa4b
Wenxuan <wenxuangm@gmail.com>
2017-04-12 18:59:13 +0800

    lots of update

---
+++ setup_osx.sh
@@ -0,0 +1,6 @@
+# https://github.com/defunkt/gist#readme
+brew install gist
+brew install primesieve # 质数生成器
+brew install pstree
+brew install wemux
+gem install tmuxinator
--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -11,6 +11,7 @@ sudo apt-get -y install make cmake
 sudo apt-get -y install python python-dev python-pip
 sudo apt-get -y install python3 python3-dev python3-pip
 sudo apt-get -y install clang clang-format
+sudo apt-get -y install ruby
 
 # OpenJDK
 sudo apt-get -y install openjdk-8-jdk openjdk-8-doc
@@ -23,7 +24,6 @@ sudo apt-get -y install git
 sudo apt-get -y install zsh
 
 sudo apt-get -y install most # Colored pager
-sudo apt-get -y install tmux
 sudo apt-get -y install vim
 sudo apt-get -y install emacs
 sudo apt-get -y install htop
@@ -34,6 +34,14 @@ sudo apt-get update
 sudo apt-get -y install neovim
 sudo pip2 install --upgrade neovim
 sudo pip3 install --upgrade neovim
+
+# Tmux, Tmuxinator and Wemux
+sudo apt-get -y install tmux
+sudo gem install tmuxinator
+sudo git clone git://github.com/zolrath/wemux.git /usr/local/share/wemux
+sudo ln -s /usr/local/share/wemux/wemux /usr/local/bin/wemux
+sudo cp /usr/local/share/wemux/wemux.conf.example /usr/local/etc/wemux.conf
+sudo cp /usr/local/share/wemux/man/wemux.1
 
 # Install oh-my-zsh if there is no one in ~ directory
 # sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
