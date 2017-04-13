a081e1db8c4ca9a37adcf55007b40dbf57c10424
Wenxuan <wenxuangm@gmail.com>
2017-04-13 17:09:03 +0800

    improve setup script
    Former-commit-id: a77b2d64e2d173c710aeb0b1658c7433badf6b68

---
+++ gradle/setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# get the dir of the current script
+script_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+# -p says to create the dir if it doesn't exist already.
+mkdir -p ~/.gradle
+ln -sf $script_dir/init.gradle ~/.gradle/init.gradle
