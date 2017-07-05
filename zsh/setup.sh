54ef0d0beb372938311eca6c61c8cb825b089ad2
Wenxuan <wenxuangm@gmail.com>
2017-07-05 09:27:45 +0800

    add missed zsh config files

--- zsh/setup.sh
+++ zsh/setup.sh
@@ -3,6 +3,10 @@
 # get the dir of the current script
 script_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-ln -sf $script_dir/zshrc ~/.zshrc
+ln -sf $script_dir/zshrc        ~/.zshrc
+ln -sf $script_dir/zshenv       ~/.zshenv
+ln -sf $script_dir/zsh_aliases  ~/.zsh_aliases
+ln -sf $script_dir/zsh_keybinds ~/.zsh_keybinds
+ln -sf $script_dir/zsh_misc     ~/.zsh_misc
 
 chsh -s `which zsh`
