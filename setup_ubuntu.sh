e0fbb8573f5c1470fd5972c64b916cf5f279b434
Wenxuan <wenxuangm@gmail.com>
2017-07-15 18:50:05 +0800

    update server config
    Former-commit-id: 01d9aaa91f46bb17c540b195b3af5a67ceba5737

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -20,6 +20,7 @@ $script_dir/python/setup.sh # config pip source mirror
 sudo apt-get -y install clang
 sudo apt-get -y install ruby
 gem sources --add https://gems.ruby-china.org/ --remove https://rubygems.org/
+sudo apt-get install libssh2-1-dev # exa need it
 
 # OpenJDK
 sudo apt-get -y install openjdk-8-jdk
