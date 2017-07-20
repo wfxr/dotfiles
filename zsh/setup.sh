1e1fc421b372171c050d771d20a094cfe7c2f885
Wenxuan <wenxuangm@gmail.com>
2017-07-20 23:42:29 +0800

    misc

--- zsh/setup.sh
+++ zsh/setup.sh
@@ -4,7 +4,7 @@
 script_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 ln -sf $script_dir/zshrc        ~/.zshrc
-ln -sf $script_dir/zshenv       ~/.zshenv
+ln -sf $script_dir/zsh_env      ~/.zsh_env
 ln -sf $script_dir/zsh_aliases  ~/.zsh_aliases
 ln -sf $script_dir/zsh_keybinds ~/.zsh_keybinds
 ln -sf $script_dir/zsh_misc     ~/.zsh_misc
