e7c87c1f6664a1fa3c71dee9c21658871ec69f8e
Wenxuan <wenxuangm@gmail.com>
2017-07-24 22:47:58 +0800

    update emacs config
    Former-commit-id: c3d45ae7690732ea71cf9f7a9beeb0deb8f94ce7

--- emacs/setup.sh
+++ emacs/setup.sh
@@ -7,4 +7,4 @@ if [[ ! -a ~/.emacs.d || ! -a ~/.emacs.d/spacemacs.mk ]]; then
     git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
 fi
 
-ln -sf $script_dir/emacs/spacemacs ~/.spacemacs
+ln -sf $script_dir/spacemacs ~/.spacemacs
