b9125773883ab962be13b932ff8217956a006676
Wenxuan <wenxuangm@gmail.com>
2017-08-11 22:05:23 +0800

    improve git-fzf config
    Former-commit-id: 7501416a65a7bb4424249b6250066b4c5cd36231

--- git/setup.sh
+++ git/setup.sh
@@ -51,3 +51,19 @@ git config --global submodule.fetchJobs 8
 # Prettier diffs
 git config --global diff.compactionHeuristic true
 git config --global diff.indentHeuristic on
+
+# diff-so-fancy
+git config --global core.pager "diff-so-fancy | less --tabs=4 -RFX"
+git config --global color.ui true
+
+git config --global color.diff-highlight.oldNormal "red bold"
+git config --global color.diff-highlight.oldHighlight "red bold 52"
+git config --global color.diff-highlight.newNormal "green bold"
+git config --global color.diff-highlight.newHighlight "green bold 22"
+
+git config --global color.diff.meta "227"
+git config --global color.diff.frag "magenta bold"
+git config --global color.diff.commit "227 bold"
+git config --global color.diff.old "red bold"
+git config --global color.diff.new "green bold"
+git config --global color.diff.whitespace "red reverse"
