a96a24636bc6756dbb3c37916476a17847078c2f
Wenxuan <wenxuangm@gmail.com>
2017-08-13 17:32:55 +0800

    minor fix
    Former-commit-id: 0dbddb74e8c13a612ca2cf652419aa7348e14503

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -20,7 +20,7 @@ $script_dir/python/setup.sh # config pip source mirror
 sudo apt-get -y install clang
 sudo apt-get -y install ruby-full
 gem sources --add https://gems.ruby-china.org/ --remove https://rubygems.org/
-sudo apt-get install libssh2-1-dev # exa need it
+sudo apt -y install libssh2-1-dev # exa need it
 
 # OpenJDK
 sudo apt-get -y install openjdk-8-jdk
