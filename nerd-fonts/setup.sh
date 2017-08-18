7707a44876f37b93163bd43b67cd9e20773f9aa5
Wenxuan <wenxuangm@gmail.com>
2017-08-18 13:37:14 +0800

    Improve vim with devicons

---
+++ nerd-fonts/setup.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+directory=.ghq/github.com/ryanoasis/
+mkdir -p $directory
+cd $directory
+git clone --depth 1 https://github.com/ryanoasis/nerd-fonts.git
+nerd-fonts/install.sh InconsolataGo
