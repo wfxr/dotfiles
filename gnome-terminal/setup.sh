31b2c2d420d9a492a1f171a7ae97cb368744af04
Wenxuan <wenxuangm@gmail.com>
2017-09-04 20:01:38 +0800

    Add gnome terminal config

---
+++ gnome-terminal/setup.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $SCRIPT_DIR
+
+# https://askubuntu.com/a/694670
+dconf load /org/gnome/terminal/ < terminal.dconf
+
+# Show theme profile with uuid
+#dconf dump /org/gnome/terminal/legacy/profiles:/ | grep -e "\[\:\|visible-name"
