fbd8ff6f8403e12c0bcb777eca0439bedfbc62e4
Wenxuan <wenxuangm@gmail.com>
2017-09-05 23:27:30 +0800

    Reset conf before load
    Former-commit-id: 619369b81c73c479143dfabd984196e874e06b68

--- gnome-terminal/setup.sh
+++ gnome-terminal/setup.sh
@@ -4,6 +4,7 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $SCRIPT_DIR
 
 # https://askubuntu.com/a/694670
+dconf reset -f /org/gnome/terminal/
 dconf load /org/gnome/terminal/ < terminal.dconf
 
 # Show theme profile with uuid
