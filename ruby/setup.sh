e4c4fe1314b5a943b398b9f5b02f8622e7638568
Wenxuan <wenxuangm@gmail.com>
2017-10-09 16:22:54 +0800

    Add rubocop config file
    Former-commit-id: f75372c13199e8f8144ea51c2062d20b97308445

--- ruby/setup.sh
+++ ruby/setup.sh
@@ -3,5 +3,8 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 gem sources --add https://gems.ruby-china.org/ --remove https://rubygems.org/
+
 gem install pry
 gem install rubocop
+
+ln -sf $SCRIPT_DIR/rubocop.yml ~/.rubocop.yml
