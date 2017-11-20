44d1eb81fe6684ce1c72127035693a9bbfb0664a
Wenxuan <wenxuangm@gmail.com>
2017-11-20 17:21:02 +0800

    Fix comment issue

--- ruby/setup.sh
+++ ruby/setup.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 gem sources --add https://gems.ruby-china.org/ --remove https://rubygems.org/
 
 gem install pry
 gem install rubocop
 
-ln -sf $SCRIPT_DIR/rubocop.yml ~/.rubocop.yml
+ln -sf "$SCRIPT_DIR"/rubocop.yml ~/.rubocop.yml
