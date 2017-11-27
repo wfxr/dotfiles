45ac5de4204784052bf49eac3ae64c08c1854e4f
Wenxuan <wenxuangm@gmail.com>
2017-11-27 16:11:44 +0800

    Add new scripts

--- backup_all.sh
+++ backup_all.sh
@@ -10,16 +10,42 @@ src=(
 .fzf.bash
 .fzf.zsh
 .tmux
+.ssh
 .gitconfig
 .ssh
 .wakatime.cfg
 .gdbinit
-dotfiles
+.cargo
+.fzf
+.fzf.zsh
+.fzf.bash
+.ghq
+.gradle
+.IntelliJIdea2017.3
+.m2
+.rustup
+.sdkman
+.Toolbox
+.local/share/JetBrains
+Downloads
+develop
+abc
 bin
+daily
+dotfiles
+IdeaProjects
+learn
+notes
+Pictures
+scripts
+report
+server
+Snapshots
+src
+work
 )
 
-excludes=(
-)
+excludes=()
 
 [ $# -gt 0 ] && target=$1 || target=backups/dotdata-backups.tgz
 
--- fonts/nerd-fonts.sh
+++ fonts/nerd-fonts.sh
@@ -4,4 +4,4 @@ directory=.ghq/github.com/ryanoasis/
 mkdir -p $directory
 cd $directory
 git clone --depth 1 https://github.com/ryanoasis/nerd-fonts.git
-nerd-fonts/install.sh InconsolataGo
+nerd-fonts/install.sh
