ad8bed5e77c78d83247321f08274e97a41aa2412
Wenxuan <wenxuangm@gmail.com>
2017-12-01 11:13:58 +0800

    Add ruby gem
    Former-commit-id: 040c9ed9fc59d2d1a0cdae5865cd5a536e1168c1

--- ruby/setup.sh
+++ ruby/setup.sh
@@ -6,5 +6,6 @@ gem sources --add https://gems.ruby-china.org/ --remove https://rubygems.org/
 
 gem install pry
 gem install rubocop
+gem install bundler
 
 ln -sf "$SCRIPT_DIR"/rubocop.yml ~/.rubocop.yml
