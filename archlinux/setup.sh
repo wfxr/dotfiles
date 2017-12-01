d014207d3293aa103ff0538ed78a0f42f1d21113
Wenxuan <wenxuangm@gmail.com>
2017-12-01 16:54:07 +0800

    Refactor setup scripts
    Former-commit-id: ec16bec8122ecdf458619a3c5833f35d69810ddd

--- archlinux/setup.sh
+++ archlinux/setup.sh
@@ -4,3 +4,6 @@ SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
 # fcitx - gnome
 ln -sf $SCRIPT_DIR/xprofile ~/.xprofile
+
+# funny programs
+pacman -S cool-retro-term # cool-retro-term is a terminal emulator which mimics the look and feel of the old cathode tube screens.
