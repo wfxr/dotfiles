f0da088a45210ae26bcaf6fe6276660518f3db3d
Wenxuan <wenxuangm@gmail.com>
2017-12-16 12:30:38 +0800

    Add odps setup script

---
+++ odps/setup.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+#===============================================================================
+#   Author: Wenxuan
+#    Email: wenxuangm@gmail.com
+#  Created: 2017-12-16 12:18
+#===============================================================================
+package=odpscmd_public.zip
+wget "http://repo.aliyun.com/download/odpscmd/latest/$package" -O "/tmp/$package" || exit $?
+mkdir -p ~/tools/odps && cd ~/tools/odps
+unzip "/tmp/$package"
+
+ln -sf ~/tools/odps/bin/odpscmd ~/bin
