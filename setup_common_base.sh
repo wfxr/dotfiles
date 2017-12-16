1c0d6b72cf3282ba50ce9fdb789163d4835c574b
Wenxuan <wenxuangm@gmail.com>
2017-12-16 13:48:52 +0800

    Add csvkit

--- setup_common_base.sh
+++ setup_common_base.sh
@@ -3,7 +3,8 @@
 [ "$(whoami)" != "root" ] && exec sudo -- "$0" "$@"
 SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
-sudo -H pip install pydf    # pydf is much easier to read
-sudo -H pip install thefuck # Thefuck is a magnificent app which corrects your previous console command
+pip install pydf    # pydf is much easier to read
+pip install thefuck # Thefuck is a magnificent app which corrects your previous console command
+pip install csvkit  # A suite of utilities for converting to and working with CSV, the king of tabular file formats.
 
 gem install lolcat
