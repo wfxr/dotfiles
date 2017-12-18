003d645cfd0c41b204395f5eca9727a934227dc6
Wenxuan <wenxuangm@gmail.com>
2017-12-18 10:03:27 +0800

    Update archlinux setup script
    Former-commit-id: 7237674518e0225112574126b4a53ad3e8485493

--- archlinux/setup.sh
+++ archlinux/setup.sh
@@ -2,8 +2,23 @@
 
 SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
-# fcitx - gnome
-ln -sf $SCRIPT_DIR/xprofile ~/.xprofile
+mv /etc/pacman.d/mirrorlist /etc/pacman/mirrorlist.bak
+cp $SCRIPT_DIR/mirrorlist /etc/pacman/mirrorlist
+
+# Download pacman mirrorlist
+# Uncomment the wanted mirror
+#wget -O/etc/pacman.d/mirrorlist https://www.archlinux.org/mirrorlist/all/
+#wget -O /etc/pacman.d/mirrorlist 'https://www.archlinux.org/mirrorlist/?country=CN'
 
 # funny programs
-pacman -S cool-retro-term # cool-retro-term is a terminal emulator which mimics the look and feel of the old cathode tube screens.
+pacman -S words
+pacman -S go
+yaourt -S jdk9-openjdk openjdk9-src
+yaourt -S pet-git
+yaourt -S mosh
+yaourt -S axel
+
+# fcitx - gnome
+pacman -S fcitx && ln -sf $SCRIPT_DIR/xprofile ~/.xprofile
+# cool-retro-term is a terminal emulator which mimics the look and feel of the old cathode tube screens.
+pacman -S cool-retro-term
