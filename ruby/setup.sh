695d9175b6ac4cc6e3e93c4f26789fcc161a6dc8
Wenxuan <wenxuangm@gmail.com>
2018-01-11 11:20:05 +0800

    Remove 'sudo' on gem command
    Former-commit-id: ee8859e554662afd2151fb78db695f03d42a1e7a

--- ruby/setup.sh
+++ ruby/setup.sh
@@ -4,8 +4,8 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 gem sources --add https://gems.ruby-china.org/ --remove https://rubygems.org/
 
-sudo -H gem install pry
-sudo -H gem install rubocop
-sudo -H gem install bundler
+gem install pry
+gem install rubocop
+gem install bundler
 
 ln -sf "$SCRIPT_DIR"/rubocop.yml ~/.rubocop.yml
