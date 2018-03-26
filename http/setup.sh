298a2800e4d2b89de8e47f868c2f91a0bfa50b6b
Wenxuan <wenxuangm@gmail.com>
2018-03-26 11:09:16 +0800

    Add http toolkit
    Former-commit-id: 5ba04ac34f3493541dab01d607642b4f3fe46fda

---
+++ http/setup.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+#===============================================================================
+#   Author: Wenxuan
+#    Email: wenxuangm@gmail.com
+#  Created: 2018-03-26 11:07
+#===============================================================================
+
+pip install --upgrade httpie
+pip install --upgrade http-prompt
