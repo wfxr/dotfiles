389960e75ef724cc684fdb334c9d3c6b4c2b8460
Wenxuan <wenxuangm@gmail.com>
2018-03-31 12:10:41 +0800

    Add docker settings
    Former-commit-id: 0f65b710c4d8b49f9156eb6fdec486dfe14a5b60

---
+++ docker/setup.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+#===============================================================================
+#   Author: Wenxuan
+#    Email: wenxuangm@gmail.com
+#  Created: 2018-03-31 11:53
+#===============================================================================
+[ "$(whoami)" != "root" ] && exec sudo -- "$0" "$@"
+SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd) && cd "$SCRIPT_DIR"
+
+mkdir -p /etc/docker
+ln -snf "$SCRIPT_DIR/daemon.json" /etc/docker/daemon.json
