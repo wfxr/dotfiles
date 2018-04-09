4dceec4c96d2d6358de88378a4f1c75a73f15d1b
Wenxuan <wenxuangm@gmail.com>
2018-04-10 00:00:31 +0800

    Update oni config
    Former-commit-id: 3eabd1ad1d72b5cdc798bfb6b9adb7e9bb17a683

--- oni/config.js
+++ oni/config.js
@@ -1,35 +1,33 @@
 "use strict";
 exports.__esModule = true;
-exports.activate = function (oni) {
+exports.activate   = function(oni) {
     console.log("config activated");
     // Input
     //
     // Add input bindings here:
     //
-    oni.input.bind("<c-enter>", function () { return console.log("Control+Enter was pressed"); });
+    oni.input.bind("<c-enter>", function() { return console.log("Control+Enter was pressed"); });
     //
     // Or remove the default bindings here by uncommenting the below line:
     //
     // oni.input.unbind("<c-p>")
 };
-exports.deactivate = function (oni) {
-    console.log("config deactivated");
-};
+exports.deactivate    = function(oni) { console.log("config deactivated"); };
 exports.configuration = {
-    //add custom config here, such as
-    //"oni.loadInitVimoni.loadInitVim": true,
-    "ui.colorscheme": "gruvbox",
-    "ui.fontSize": "16px",
-    "oni.hideMenu": true,
-    "wildmenu.mode": false,
-    "commandline.mode": false,
-    "oni.useDefaultConfig": true,
-    //"oni.bookmarks": ["~/Documents"],
-    "oni.loadInitVim": true,
-    "editor.fontSize": "18px",
-    "editor.completions.mode": "native",
-    //"editor.fontFamily": "Monaco",
+    "sidebar.enabled" : false,
+    "ui.colorscheme" : "gruvbox",
+    "ui.fontSize" : "16px",
+    "oni.hideMenu" : true,
+    "wildmenu.mode" : false,
+    "commandline.mode" : false,
+    "oni.useDefaultConfig" : true,
+    "browser.defaultUrl" : "https://github.com",
+    "oni.loadInitVim" : true,
+    "editor.fontSize" : "16px",
+    "editor.completions.mode" : "native", // [native, oni, hidden]
+    "tabs.mode" : "hidden",
+    //"editor.fontFamily": "DejaVu Sans Mono",
     // UI customizations
-    "ui.animations.enabled": true,
-    "ui.fontSmoothing": "auto"
+    "ui.animations.enabled" : true,
+    "ui.fontSmoothing" : "auto"
 };
