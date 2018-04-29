43fc55cf4cabfc717238a39c6f5ad9b11f13efc2
Wenxuan <wenxuangm@gmail.com>
2018-04-29 21:49:11 +0800

    Improve rust config
    Former-commit-id: 74014c775ac8fe5acf368da2d5ea0b291fc889de

--- rust/setup.sh
+++ rust/setup.sh
@@ -4,10 +4,10 @@
 SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
 hash rustup &>/dev/null || curl https://sh.rustup.rs -sSf | sh || exit 1
-rustup component add rust-src
+hash rustfmt &>/dev/null || cargo install rustfmt
+rustup component list | grep rust-src &>/dev/null || rustup component add rust-src
 
 mkdir -p ~/.cargo
 
 ln -snf "$SCRIPT_DIR/config" ~/.cargo/config
-
-cargo install rustfmt
+ln -snf "$SCRIPT_DIR/rustfmt.toml" ~/.rustfmt.toml
