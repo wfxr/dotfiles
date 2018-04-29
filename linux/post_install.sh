c6c71e78d8bf7b5a251f76ecf049fbc9ebaa9a81
Wenxuan <wenxuangm@gmail.com>
2018-04-29 03:38:31 +0000

    Disable some software
    Former-commit-id: 7474514699102793248bc3843e208fa00bd3dc88

--- linux/post_install.sh
+++ linux/post_install.sh
@@ -5,11 +5,11 @@ SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
 pip install --upgrade pip
 pip install pydf         # pydf is much easier to read
-pip install thefuck      # Thefuck is a magnificent app which corrects your previous console command
+#pip install thefuck      # Thefuck is a magnificent app which corrects your previous console command
 pip install csvkit       # A suite of utilities for converting to and working with CSV, the king of tabular file formats.
 pip install jsbeautifier # json formater
 
 gem sources --add https://gems.ruby-china.org/ --remove https://rubygems.org/
 gem update --system
-gem install tmuxinator
+#gem install tmuxinator
 gem install lolcat
