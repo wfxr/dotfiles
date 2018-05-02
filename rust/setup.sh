fefb830ad5c933c1aca815b745e060432489a5f9
Wenxuan <wenxuangm@gmail.com>
2018-05-02 21:29:53 +0800

    Improve rust setup script
    Former-commit-id: d555c78f1a3781d13c542721252f9abf13832a0b

--- rust/setup.sh
+++ rust/setup.sh
@@ -3,6 +3,9 @@
 # get the dir of the current script
 SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
+ln -snf "$SCRIPT_DIR/config" ~/.cargo/config
+ln -snf "$SCRIPT_DIR/rustfmt.toml" ~/.rustfmt.toml
+
 hash rustup &>/dev/null || curl https://sh.rustup.rs -sSf | sh || exit 1
 rustup update
 rustup component add rls-preview rust-analysis rust-src rustfmt-preview
@@ -11,6 +14,3 @@ mkdir -p ~/.zsh_completions
 rustup completions zsh > ~/.zsh_completions/_rustup
 
 mkdir -p ~/.cargo
-
-ln -snf "$SCRIPT_DIR/config" ~/.cargo/config
-ln -snf "$SCRIPT_DIR/rustfmt.toml" ~/.rustfmt.toml
