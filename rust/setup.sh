382e0c4596172d467440145d877314c77e13c338
Wenxuan <wenxuangm@gmail.com>
2018-05-03 17:20:25 +0800

    :bug: Fix
    Former-commit-id: 13a465e0abaa318a9de9752e8f50e67388e182fd

--- rust/setup.sh
+++ rust/setup.sh
@@ -3,6 +3,7 @@
 # get the dir of the current script
 SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
+mkdir -p ~/.cargo
 ln -snf "$SCRIPT_DIR/config" ~/.cargo/config
 ln -snf "$SCRIPT_DIR/rustfmt.toml" ~/.rustfmt.toml
 
@@ -12,5 +13,3 @@ rustup component add rls-preview rust-analysis rust-src rustfmt-preview
 
 mkdir -p ~/.zsh_completions
 rustup completions zsh > ~/.zsh_completions/_rustup
-
-mkdir -p ~/.cargo
