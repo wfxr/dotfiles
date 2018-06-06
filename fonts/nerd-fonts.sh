648d18477749351a3d9b4ea507d88dfe7274f456
Wenxuan <wenxuangm@gmail.com>
2018-06-06 17:53:46 +0800

    Update nerd fonts install script
    Former-commit-id: 2c579731074eea754da204901670a2041d4f207d

--- fonts/nerd-fonts.sh
+++ fonts/nerd-fonts.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 directory=.ghq/github.com/ryanoasis/
-mkdir -p $directory
-cd $directory
-git clone --depth 1 https://github.com/ryanoasis/nerd-fonts.git
+mkdir -p $directory && cd $directory || exit 1
+git clone --depth 1 https://github.com/ryanoasis/nerd-fonts || cd nerd-fonts || exit 1
+git pull
 nerd-fonts/install.sh
