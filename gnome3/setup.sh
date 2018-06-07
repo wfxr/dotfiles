ecf1aa28b200beb2f5fc09d08b13309bc336c367
Wenxuan <wenxuangm@gmail.com>
2018-06-07 12:29:18 +0800

    Refactor gnome config
    Former-commit-id: 3885aff3531263b30333513c808df1b1b0b55a6c

--- gnome3/setup.sh
+++ gnome3/setup.sh
@@ -3,8 +3,16 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" && cd "$SCRIPT_DIR" || return 1
 
 # https://askubuntu.com/a/694670
-dconf reset -f /org/gnome/terminal/
-dconf load /org/gnome/terminal/ < terminal.dconf
+function dconf-reload() {
+    [[ -f "$2" ]] || return 1
+    dconf reset -f "$1"
+    dconf load "$1" < "$2"
+}
+
+dconf-reload /org/gnome/terminal/        terminal.dconf
+dconf-reload /org/gnome/settings-daemon/ settings-deamon.dconf
+dconf-reload /org/gnome/mutter/          mutter.dconf
+dconf-reload /org/gnome/desktop/wm/      desktop-wm.dconf
 
 # Show theme profile with uuid
 #dconf dump /org/gnome/terminal/legacy/profiles:/ | grep -e "\[\:\|visible-name"
