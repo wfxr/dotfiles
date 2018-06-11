83cc1d2605017362528899e22bca0ec0f0983de2
Wenxuan <wenxuangm@gmail.com>
2018-06-11 17:19:04 +0800

    Cpp: Install cppman from pip
    Former-commit-id: 443c26761c0aec81f07b5718cfd9f66ba6e87bab

--- cpp/setup.sh
+++ cpp/setup.sh
@@ -7,5 +7,5 @@ ln -sf "$SCRIPT_DIR/clang-format"      ~/.clang-format
 [[ -f ~/.gdbinit ]] || wget -P ~ git.io/.gdbinit
 
 # CLI cpp reference
-sudo -H pip install pygments
-sudo pacman -S cppman-git 2>/dev/null
+sudo -H pip install --upgrade pygments
+sudo -H pip install --upgrade cppman
