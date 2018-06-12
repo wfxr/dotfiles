4898fe614a10beb6495d9675227ff8ee81821ba4
Wenxuan <wenxuangm@gmail.com>
2018-06-12 20:44:57 +0800

    Ubuntu: Improve neovim setup script
    Former-commit-id: 6806d069ed3bde2733b1793f70c4cc545518789c

--- ubuntu/setup_neovim.sh
+++ ubuntu/setup_neovim.sh
@@ -3,9 +3,9 @@
 [ "$(whoami)" != "root" ] && exec sudo -- "$0" "$@"
 SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
+apt update && apt -y install software-properties-common
 add-apt-repository -y ppa:neovim-ppa/unstable
-apt update
-apt -y install neovim
+apt update && apt -y install neovim
 hash gem  &>/dev/null && gem  install neovim
 hash pip2 &>/dev/null && pip2 install --upgrade neovim
 hash pip3 &>/dev/null && pip3 install --upgrade neovim
