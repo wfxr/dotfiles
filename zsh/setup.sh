b7ae29c3261fa8b0c1efb79bbe0ad9068b82c7eb
Wenxuan <wenxuangm@gmail.com>
2018-06-13 15:10:54 +0800

    FZF: Add custom patch
    Former-commit-id: a9fa08c7ad94000db26bc92ee1d3565692dea54e

--- zsh/setup.sh
+++ zsh/setup.sh
@@ -3,14 +3,15 @@
 # get the dir of the current script
 SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd) && cd "$SCRIPT_DIR" || return 1
 
-ln -sf "$SCRIPT_DIR/zshrc"        ~/.zshrc
-ln -sf "$SCRIPT_DIR/zsh_env"      ~/.zsh_env
-ln -sf "$SCRIPT_DIR/zsh_aliases"  ~/.zsh_aliases
-ln -sf "$SCRIPT_DIR/zsh_keybinds" ~/.zsh_keybinds
-ln -sf "$SCRIPT_DIR/zsh_misc"     ~/.zsh_misc
-ln -sf "$SCRIPT_DIR/zsh_custom"   ~/.zsh_custom
-ln -sf "$SCRIPT_DIR/zsh_plug"     ~/.zsh_plug
-ln -sf "$SCRIPT_DIR/zsh_theme"    ~/.zsh_theme
+ln -sf "$SCRIPT_DIR/zshrc"         ~/.zshrc
+ln -sf "$SCRIPT_DIR/zsh_env"       ~/.zsh_env
+ln -sf "$SCRIPT_DIR/zsh_aliases"   ~/.zsh_aliases
+ln -sf "$SCRIPT_DIR/zsh_keybinds"  ~/.zsh_keybinds
+ln -sf "$SCRIPT_DIR/zsh_misc"      ~/.zsh_misc
+ln -sf "$SCRIPT_DIR/zsh_custom"    ~/.zsh_custom
+ln -sf "$SCRIPT_DIR/zsh_plug"      ~/.zsh_plug
+ln -sf "$SCRIPT_DIR/zsh_theme"     ~/.zsh_theme
+ln -sf "$SCRIPT_DIR/zsh_fzf_extra" ~/.zsh_fzf_extra
 
 mkdir -p ~/.zsh_completions
 ln -sf "$SCRIPT_DIR/completions/_ag"  ~/.zsh_completions/_ag
