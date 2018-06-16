ac15f0e5ecc6ef2b9cdf1c4248e24185015e1713
Wenxuan <wenxuangm@gmail.com>
2018-06-16 19:22:57 +0800

    Emacs: Update config
    Former-commit-id: ca4c58c64b493f695263ade3a7ade077da34473b

--- emacs/setup.sh
+++ emacs/setup.sh
@@ -3,7 +3,7 @@
 SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
 if [[ ! -a ~/.emacs.d || ! -a ~/.emacs.d/spacemacs.mk ]]; then
-    git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
+    git clone --depth=1 https://github.com/syl20bnr/spacemacs ~/.emacs.d
 fi
 
 ln -sf "$SCRIPT_DIR/spacemacs" ~/.spacemacs
