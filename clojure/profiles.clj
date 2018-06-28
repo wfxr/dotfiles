325c23314382b759d20c6e028e9154aad6b3db4c
Wenxuan <wenxuangm@gmail.com>
2018-06-29 00:01:59 +0800

    Clojure: Update setup script.
    Former-commit-id: 7918b39cc7e871e9f536929e1db4d72a7ba2870d

---
+++ clojure/profiles.clj
@@ -0,0 +1,3 @@
+{:user {:plugins [[lein-pprint "1.2.0"]
+                  [lein-exec "0.3.7"]
+                  [lein-kibit "0.1.6"]]}}
--- clojure/setup.sh
+++ clojure/setup.sh
@@ -1,6 +1,9 @@
-#!/bin/bash
+#!/usr/bin/env bash
+SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd) && cd "$SCRIPT_DIR" || exit 1
 
-if [[ ! -a ~/bin/lein ]]; then
-    wget https://raw.githubusercontent.com/technomancy/leiningen/stable/bin/lein -O ~/bin/lein
+[[ ! -a ~/bin/lein ]] &&
+    wget https://raw.githubusercontent.com/technomancy/leiningen/stable/bin/lein -O ~/bin/lein &&
     chmod +x ~/bin/lein
-fi
+
+mkdir -p ~/.lein
+ln -sf "$SCRIPT_DIR/profiles.clj" ~/.lein/profiles.clj
