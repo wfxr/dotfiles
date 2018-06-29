d569b134aeebcaeef60fab1a41b75a41603ccbff
Wenxuan <wenxuangm@gmail.com>
2018-06-29 14:13:34 +0800

    Clojure: Add lein plugins.
    Former-commit-id: 89e3c9fc7ce280dbb39142b2fa9838277551b3ea

--- clojure/profiles.clj
+++ clojure/profiles.clj
@@ -1,3 +1,5 @@
 {:user {:plugins [[lein-pprint "1.2.0"]
                   [lein-exec "0.3.7"]
-                  [lein-kibit "0.1.6"]]}}
+                  [lein-kibit "0.1.6"]
+                  [lein-cprint "1.3.0"]
+                  [lein-cljfmt "0.5.7"]]}}
