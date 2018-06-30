01f4e6406711db91ed419d5055c15a5c5bfbd3c7
Wenxuan <wenxuangm@gmail.com>
2018-06-30 10:51:55 +0800

    Clojure: Clean plugins.
    Former-commit-id: be38625665ccc6f1e2f55868c435f3620c088355

--- clojure/profiles.clj
+++ clojure/profiles.clj
@@ -1,5 +1,4 @@
 {:user {:plugins [[lein-pprint "1.2.0"]
                   [lein-exec "0.3.7"]
                   [lein-kibit "0.1.6"]
-                  [lein-cprint "1.3.0"]
                   [lein-cljfmt "0.5.7"]]}}
