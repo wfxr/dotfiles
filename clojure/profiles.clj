2a395ac62fcd9e73388696e12242182d99b18468
Wenxuan <wenxuangm@gmail.com>
2018-07-24 14:56:47 +0800

    Clojure: Add nrepl to lein plugins.
    Former-commit-id: e1103ff68d98e021e8db00e6cd78db3f2a201b09

--- clojure/profiles.clj
+++ clojure/profiles.clj
@@ -1,4 +1,5 @@
 {:user {:plugins [[lein-pprint "1.2.0"]
                   [lein-exec "0.3.7"]
                   [lein-kibit "0.1.6"]
-                  [lein-cljfmt "0.5.7"]]}}
+                  [lein-cljfmt "0.5.7"]
+                  [cider/cider-nrepl "0.17.0"]]}}
