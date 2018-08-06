fbb5274448fdb40cfbe20b17d31baa1c2c36b10c
Wenxuan <wenxuangm@gmail.com>
2018-08-06 13:10:14 +0800

    Clojure: add lein plugin.
    Former-commit-id: 07125ac82970b77acc07479df4496ada271b7db8

--- clojure/profiles.clj
+++ clojure/profiles.clj
@@ -2,4 +2,5 @@
                   [lein-exec "0.3.7"]
                   [lein-kibit "0.1.6"]
                   [lein-cljfmt "0.5.7"]
-                  [cider/cider-nrepl "0.17.0"]]}}
+                  [cider/cider-nrepl "0.17.0"]
+                  [mvxcvi/whidbey "1.3.2"]]}}
