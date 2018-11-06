7932e425b64a47542079fc338c6c70d3e4c14d73
Wenxuan <wenxuangm@gmail.com>
2018-11-06 16:19:54 +0800

    Update gem mirror.
    Former-commit-id: 276cbb3b6e2f17c49c1180278c0d5bd998049910

--- ruby/setup.sh
+++ ruby/setup.sh
@@ -2,7 +2,7 @@
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-gem sources --add https://gems.ruby-china.org/ --remove https://rubygems.org/
+gem sources --add https://gems.ruby-china.com --remove https://rubygems.org/
 
 gem install pry
 gem install rubocop
