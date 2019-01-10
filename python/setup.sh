a618d61bef4f2e2da4d3ee959a8993119f811b17
Wenxuan <wenxuangm@gmail.com>
2019-01-10 15:43:47 +0800

    Python: Improve config.
    Former-commit-id: 852bd1c68a01327edad4a4d99db61278aaa72d47

--- python/setup.sh
+++ python/setup.sh
@@ -4,11 +4,12 @@ SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
 mkdir -p ~/.pip
 ln -sf "$SCRIPT_DIR/pip.conf" ~/.pip/pip.conf
-sudo -H pip install --upgrade pip
+pip install --upgrade pip
 
 # Formatter / Repl
-sudo -H pip install yapf ptpython
+pip install yapf ptpython
 
 # Config for ptpython
 mkdir -p ~/.ptpython
 ln -sf "$SCRIPT_DIR/ptpythonrc" ~/.ptpython/config.py
+ln -sf "$SCRIPT_DIR/style.yapf" ~/.style.yapf
