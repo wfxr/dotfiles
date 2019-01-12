2cfeaae01f9e9498d3ec163f472dbc9bb2a1b35e
Wenxuan <wenxuangm@gmail.com>
2019-01-12 23:59:20 +0800

    ArchLinux: Tons of improvements.
    Former-commit-id: c7fe8ebbe5bfee5f8fd5f28ea998cc5c85e3b96c

--- arch/setup.sh
+++ arch/setup.sh
@@ -1,53 +1,63 @@
 #!/usr/bin/env bash
+[ "$(whoami)" != "root" ] && exec sudo -- "$0" "$@"
 
-SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd) && cd "$SCRIPT_DIR" || exit 1
 
-mv /etc/pacman.d/mirrorlist  /etc/pacman/mirrorlist.bak
-cp "$SCRIPT_DIR/mirrorlist"  /etc/pacman/mirrorlist
+mv /etc/pacman.d/mirrorlist  /etc/pacman.d/mirrorlist.bak
+cp "$SCRIPT_DIR/mirrorlist"  /etc/pacman.d/mirrorlist
 mv /etc/pacman.conf          /etc/pacman.conf.bak
 cp "$SCRIPT_DIR/pacman.conf" /etc/pacman.conf
+pacman --noconfirm -Sy archlinuxcn-keyring yay
 
-ln -sf "$SCRIPT_DIR/yaourtrc" ~/.yaourtrc
+#ln -sf "$SCRIPT_DIR/yaourtrc" ~/.yaourtrc
 
 # Download pacman mirrorlist
 # Uncomment the wanted mirror
 #wget -O/etc/pacman.d/mirrorlist https://www.archlinux.org/mirrorlist/all/
 #wget -O /etc/pacman.d/mirrorlist 'https://www.archlinux.org/mirrorlist/?country=CN'
 
-pacman -S words
-pacman -S go
-pacman -S perl-rename
-# yaourt -S jdk9-openjdk openjdk9-src
-yaourt -S pet-git
-# yaourt -S mosh
-yaourt -S axel
-yaourt -S shellcheck
-yaourt -S highlight
-yaourt -S jq
-yaourt -S pv
-yaourt -S pigz
-yaourt -S nethogs
-yaourt -S icdiff # better diff
-# yaourt -S global # gtags
-yaourt -S universal-ctags-git # better ctags
-yaourt -S ncdu   # NCurses Disk Usage
-# yaourt -S create_ap # Create AP
-pacman -S fd
-pacman -S exa
-pacman -S bat
+pacman --noconfirm -S openssh make cmake
+pacman --noconfirm -S clang go pyenv
+pacman --noconfirm -S zsh htop
+pacman --noconfirm -S words
+pacman --noconfirm -S perl-rename
+# pacman --noconfirm -S jdk9-openjdk openjdk9-src
+# pacman --noconfirm -S pet-git
+# pacman --noconfirm -S mosh
+pacman --noconfirm -S axel
+pacman --noconfirm -S shellcheck
+pacman --noconfirm -S highlight
+pacman --noconfirm -S jq pv pigz
+pacman --noconfirm -S nethogs
+pacman --noconfirm -S icdiff # better diff
+# pacman --noconfirm -S global # gtags
+pacman --noconfirm -S universal-ctags-git # better ctags
+pacman --noconfirm -S ncdu   # NCurses Disk Usage
+# pacman --noconfirm -S create_ap # Create AP
+pacman --noconfirm -S fd exa bat
+
+pacman --noconfirm -S shadowsocks-libev ipset
+systemctl daemon-reload
+systemctl enable ipset
+systemctl start  ipset
+
+
+pacman --noconfirm -S xclip
 
 # crontab
-pacman -S cronie
+pacman --noconfirm -S cronie
+systemctl daemon-reload
 systemctl enable cronie.service
 systemctl start  cronie.service
 
 # fcitx - gnome
-pacman -S fcitx && ln -sf "$SCRIPT_DIR/xprofile" ~/.xprofile
+pacman --noconfirm -S fcitx fcitx-gtk2 fcitx-gtk3 fcitx-qt4 fcitx-qt5 fcitx-configtool fcitx-sogoupinyin
+ln -sf "$SCRIPT_DIR/xprofile" ~/.xprofile
 # cool-retro-term is a terminal emulator which mimics the look and feel of the old cathode tube screens.
-pacman -S cool-retro-term
+pacman --noconfirm -S cool-retro-term
 
 # Auto clean pacman cache
-pacman -S pacman-contrib
+pacman --noconfirm -S pacman-contrib
 systemctl daemon-reload
 systemctl enable paccache.timer
-systemctl start paccache.timer
+systemctl start  paccache.timer
