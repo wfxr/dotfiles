1060ab2e56164ce7197dd8f872474550bf51ab9e
Wenxuan <wenxuangm@gmail.com>
2019-01-18 18:42:46 +0800

    Bat: Add theme setup.
    Former-commit-id: 060a90659f285af20c2165c5aa80201754ca92fc

--- bat/setup.sh
+++ bat/setup.sh
@@ -8,3 +8,9 @@ SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd) && cd "$SCRIPT_DIR" || 
 
 mkdir -p ~/.config/bat
 ln -sf "$SCRIPT_DIR/config" ~/.config/bat/config
+
+# Themes
+mkdir -p "$(bat cache --config-dir)/themes"
+cd "$(bat cache --config-dir)/themes" || exit 1
+git clone --depth=1 https://github.com/Briles/gruvbox.git
+bat cache --init
