c9a967071f64d1a1c0d1d8439bd4a36827c47330
Wenxuan <wenxuangm@gmail.com>
2019-01-18 15:11:34 +0800

    Script: Update color script.
    Former-commit-id: 3c953021473b014b1e1af9a0dccbb46789520fec

--- scripts/color.sh
+++ scripts/color.sh
@@ -1,53 +1,43 @@
 #!/bin/env bash
 
-COLOR_NC='\e[0m' # No Color
-COLOR_WHITE='\e[1;37m'
-COLOR_BLACK='\e[0;30m'
-COLOR_BLUE='\e[0;34m'
-COLOR_LIGHT_BLUE='\e[1;34m'
-COLOR_GREEN='\e[0;32m'
-COLOR_LIGHT_GREEN='\e[1;32m'
-COLOR_CYAN='\e[0;36m'
-COLOR_LIGHT_CYAN='\e[1;36m'
-COLOR_RED='\e[0;31m'
-COLOR_LIGHT_RED='\e[1;31m'
-COLOR_PURPLE='\e[0;35m'
-COLOR_LIGHT_PURPLE='\e[1;35m'
-COLOR_BROWN='\e[0;33m'
-COLOR_YELLOW='\e[1;33m'
-COLOR_GRAY='\e[0;30m'
-COLOR_LIGHT_GRAY='\e[0;37m'
+export RST='\e[0m'   # Reset
 
-txtblk='\e[0;30m' # Black - Regular
-txtred='\e[0;31m' # Red
-txtgrn='\e[0;32m' # Green
-txtylw='\e[0;33m' # Yellow
-txtblu='\e[0;34m' # Blue
-txtpur='\e[0;35m' # Purple
-txtcyn='\e[0;36m' # Cyan
-txtwht='\e[0;37m' # White
-bldblk='\e[1;30m' # Black - Bold
-bldred='\e[1;31m' # Red
-bldgrn='\e[1;32m' # Green
-bldylw='\e[1;33m' # Yellow
-bldblu='\e[1;34m' # Blue
-bldpur='\e[1;35m' # Purple
-bldcyn='\e[1;36m' # Cyan
-bldwht='\e[1;37m' # White
-unkblk='\e[4;30m' # Black - Underline
-undred='\e[4;31m' # Red
-undgrn='\e[4;32m' # Green
-undylw='\e[4;33m' # Yellow
-undblu='\e[4;34m' # Blue
-undpur='\e[4;35m' # Purple
-undcyn='\e[4;36m' # Cyan
-undwht='\e[4;37m' # White
-bakblk='\e[40m'   # Black - Background
-bakred='\e[41m'   # Red
-bakgrn='\e[42m'   # Green
-bakylw='\e[43m'   # Yellow
-bakblu='\e[44m'   # Blue
-bakpur='\e[45m'   # Purple
-bakcyn='\e[46m'   # Cyan
-bakwht='\e[47m'   # White
-txtrst='\e[0m'    # Text Reset
+# Regular
+export BLK='\e[0;30m' # Black
+export RED='\e[0;31m' # Red
+export GRN='\e[0;32m' # Green
+export YLW='\e[0;33m' # Yellow
+export BLU='\e[0;34m' # Blue
+export PUR='\e[0;35m' # Purple
+export CYN='\e[0;36m' # Cyan
+export WHT='\e[0;37m' # White
+
+# Bold
+export BBLK='\e[1;30m' # Black
+export BRED='\e[1;31m' # Red
+export BGRN='\e[1;32m' # Green
+export BYLW='\e[1;33m' # Yellow
+export BBLU='\e[1;34m' # Blue
+export BPUR='\e[1;35m' # Purple
+export BCYN='\e[1;36m' # Cyan
+export BWHT='\e[1;37m' # White
+
+# Underline
+export UBLK='\e[4;30m' # Black
+export URED='\e[4;31m' # Red
+export UGRN='\e[4;32m' # Green
+export UYLW='\e[4;33m' # Yellow
+export UBLU='\e[4;34m' # Blue
+export UPUR='\e[4;35m' # Purple
+export UCYN='\e[4;36m' # Cyan
+export UWHT='\e[4;37m' # White
+
+# Background
+export BGBLK='\e[40m'   # Black
+export BGRED='\e[41m'   # Red
+export BGGRN='\e[42m'   # Green
+export BGYLW='\e[43m'   # Yellow
+export BGBLU='\e[44m'   # Blue
+export BGPUR='\e[45m'   # Purple
+export BGCYN='\e[46m'   # Cyan
+export BGWHT='\e[47m'   # White
