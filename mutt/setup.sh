77b6206602859b8d7d5541c77bc28afd587e6db3
Wenxuan <wenxuangm@gmail.com>
2019-01-23 23:42:36 +0800

    Mutt: Update config.
    Former-commit-id: 0906ff9af0eeea36459e80b9f3f68682e676d1d6

--- mutt/setup.sh
+++ mutt/setup.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
-#===============================================================================
-#   Author: Wenxuan
-#    Email: wenxuangm@gmail.com
-#  Created: 2018-03-01 16:49
-#===============================================================================
-
+################################################################################
+#    Author: Wenxuan                                                           #
+#     Email: wenxuangm@gmail.com                                               #
+#   Created: 2019-01-23 18:46                                                  #
+################################################################################
 SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
-curl https://raw.githubusercontent.com/altercation/mutt-colors-solarized/master/mutt-colors-solarized-light-256.muttrc > ~/.mutt_theme
-cp "$SCRIPT_DIR/muttrc" ~/.muttrc
+ln -sf "$SCRIPT_DIR/mutt_theme" ~/.mutt_theme
 ln -sf "$SCRIPT_DIR/mutt_binds" ~/.mutt_binds
+ln -sf "$SCRIPT_DIR/muttrc.pvt" ~/.muttrc
