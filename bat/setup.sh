e025060e45aa19ed4f0d27381f92f37ac18a802f
Wenxuan <wenxuangm@gmail.com>
2019-02-15 18:12:54 +0800

    Bat: Update setup script.
    Former-commit-id: b90c901bf81a3c9b5bc831931c81472352dcacfb

--- bat/setup.sh
+++ bat/setup.sh
@@ -10,8 +10,8 @@ mkdir -p ~/.config/bat
 ln -sf "$SCRIPT_DIR/config" ~/.config/bat/config
 
 # Themes
-mkdir -p "$(bat cache --config-dir)/themes"
-cd "$(bat cache --config-dir)/themes" || exit 1
+mkdir -p "$(bat --config-dir)/themes"
+cd "$(bat --config-dir)/themes" || exit 1
 test ! -d gruvbox &&
     git clone --depth=1 https://github.com/Briles/gruvbox.git
-bat cache --init
+bat cache --build
