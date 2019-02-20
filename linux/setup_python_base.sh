748e5be34542e7f2179f222248ec8b4d7262cfa7
Wenxuan Zhang <wenxuangm@gmail.com>
2019-02-20 12:20:13 +0800

    Improve setup_python_base.sh.
    Former-commit-id: ba8006cf78e6ba83c77dd7d408265ce592396abc

--- linux/setup_python_base.sh
+++ linux/setup_python_base.sh
@@ -4,14 +4,24 @@
 #     Email: wenxuangm@gmail.com                                               #
 #   Created: 2019-02-20 11:25                                                  #
 ################################################################################
+set -euo pipefail
+IFS=$'\n\t'
 
 # pyenv
-curl https://pyenv.run | bash
+hash pyenv &>/dev/null || curl https://pyenv.run | bash
 export PATH="$HOME/.pyenv/bin:$PATH"
 eval "$(pyenv init -)"
 eval "$(pyenv virtualenv-init -)"
 export PYTHON_CONFIGURE_OPTS="--enable-shared"
-v=3.7.0 && wget http://mirrors.sohu.com/python/$v/Python-$v.tar.xz -P ~/.pyenv/cache/ && pyenv install $v
+
+# python
+v=3.7.0
+pkg=Python-$v.tar.xz
+mkdir -p ~/.pyenv/cache
+[[ ! -f ~/.pyenv/cache/$pkg ]] &&
+    wget http://mirrors.sohu.com/python/$v/$pkg -P /tmp &&
+    mv /tmp/$pkg ~/.pyenv/cache/
+pyenv install $v
 
 # pypi
 mkdir -p "$HOME/.pip"
