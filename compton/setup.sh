1bbac065ad9a875d643ac0e15020ddfddf2d38c5
Wenxuan Zhang <wenxuangm@gmail.com>
2019-12-03 11:55:01 +0800

    Compton: Add compton config.
    Former-commit-id: 83a1149a67712bbe2a3c098b0ef9ea67959c35ce

---
+++ compton/setup.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+################################################################################
+#    Author: Wenxuan Zhang                                                     #
+#     Email: wenxuangm@gmail.com                                               #
+#   Created: 2019-12-01 10:33                                                  #
+################################################################################
+set -euo pipefail
+IFS=$'\n\t'
+
+SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd) && cd "$SCRIPT_DIR" || return 1
+
+mkdir -p ~/.config
+ln -sf "$SCRIPT_DIR/compton.conf" ~/.config/compton.conf
