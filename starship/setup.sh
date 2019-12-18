7a5b0fec3e0bc854cecf9432e3ab68c4e25c5ca2
Wenxuan Zhang <wenxuangm@gmail.com>
2019-12-18 11:32:56 +0800

    Zsh: Try faster starship theme.
    Former-commit-id: 263affa9a1c5f6a7c61bf0a4f82f9d35c4d7c9ea

---
+++ starship/setup.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+################################################################################
+#    Author: Wenxuan Zhang                                                     #
+#     Email: wenxuangm@gmail.com                                               #
+#   Created: 2019-12-18 11:06                                                  #
+################################################################################
+
+info() { printf "%b[info]%b %s\n" '\e[0;32m' '\e[0m' "$@" >&2; }
+
+SDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd) && cd "$SDIR" || exit 1
+
+mkdir -p ~/.config
+ln -sf "$SDIR/starship.toml" ~/.config/
+
+hash starship &>/dev/null && info "starship already installed." && exit 0
+
+mkdir -p ~/bin
+curl -fsSL https://starship.rs/install.sh | bash -s -- -b ~/bin
--- zsh/setup.sh
+++ zsh/setup.sh
@@ -1,25 +1,27 @@
 #!/usr/bin/env bash
 
 # get the dir of the current script
-SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd) && cd "$SCRIPT_DIR" || return 1
+SDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd) && cd "$SDIR" || return 1
 
-ln -sf "$SCRIPT_DIR/zshrc"         ~/.zshrc
-ln -sf "$SCRIPT_DIR/zsh_env"       ~/.zsh_env
-ln -sf "$SCRIPT_DIR/zsh_aliases"   ~/.zsh_aliases
-ln -sf "$SCRIPT_DIR/zsh_keybinds"  ~/.zsh_keybinds
-ln -sf "$SCRIPT_DIR/zsh_misc"      ~/.zsh_misc
-ln -sf "$SCRIPT_DIR/zsh_custom"    ~/.zsh_custom
-ln -sf "$SCRIPT_DIR/zsh_plug"      ~/.zsh_plug
-ln -sf "$SCRIPT_DIR/zsh_theme"     ~/.zsh_theme
-ln -sf "$SCRIPT_DIR/zsh_fzf_extra" ~/.zsh_fzf_extra
-ln -sf "$SCRIPT_DIR/zsh_secret"    ~/.zsh_secret
+ln -sf "$SDIR/zshrc"         ~/.zshrc
+ln -sf "$SDIR/zsh_env"       ~/.zsh_env
+ln -sf "$SDIR/zsh_aliases"   ~/.zsh_aliases
+ln -sf "$SDIR/zsh_keybinds"  ~/.zsh_keybinds
+ln -sf "$SDIR/zsh_misc"      ~/.zsh_misc
+ln -sf "$SDIR/zsh_custom"    ~/.zsh_custom
+ln -sf "$SDIR/zsh_plug"      ~/.zsh_plug
+# ln -sf "$SDIR/zsh_theme"     ~/.zsh_theme
+ln -sf "$SDIR/zsh_fzf_extra" ~/.zsh_fzf_extra
+ln -sf "$SDIR/zsh_secret"    ~/.zsh_secret
 
 mkdir -p ~/.zsh_completions
-ln -sf "$SCRIPT_DIR/completions/_ag"  ~/.zsh_completions/_ag
-ln -sf "$SCRIPT_DIR/completions/_pet" ~/.zsh_completions/_pet
-ln -sf "$SCRIPT_DIR/completions/_jq"  ~/.zsh_completions/_jq
-ln -sf "$SCRIPT_DIR/completions/_fzf" ~/.zsh_completions/_fzf
-ln -sf "$SCRIPT_DIR/completions/_gi"  ~/.zsh_completions/_gi
-ln -sf "$SCRIPT_DIR/completions/_hub" ~/.zsh_completions/_hub
+ln -sf "$SDIR/completions/_ag"  ~/.zsh_completions/_ag
+ln -sf "$SDIR/completions/_pet" ~/.zsh_completions/_pet
+ln -sf "$SDIR/completions/_jq"  ~/.zsh_completions/_jq
+ln -sf "$SDIR/completions/_fzf" ~/.zsh_completions/_fzf
+ln -sf "$SDIR/completions/_gi"  ~/.zsh_completions/_gi
+ln -sf "$SDIR/completions/_hub" ~/.zsh_completions/_hub
+
+../starship/setup.sh
 
 [[ "$SHELL" =~ "zsh" ]] || chsh -s "$(command -v zsh)"
