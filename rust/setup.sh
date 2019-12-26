caefac6f7811de10c79583e62864fbfb58d102a1
Wenxuan Zhang <wenxuangm@gmail.com>
2019-12-26 10:10:22 +0800

    Rust: Update setup script
    Former-commit-id: 1b56572c67935de0992cfbde4204950c4f230d20

--- rust/setup.sh
+++ rust/setup.sh
@@ -7,10 +7,13 @@ mkdir -p ~/.cargo
 ln -snf "$SCRIPT_DIR/config" ~/.cargo/config
 ln -snf "$SCRIPT_DIR/rustfmt.toml" ~/.rustfmt.toml
 
-hash rustup &>/dev/null || curl https://sh.rustup.rs -sSf | sh || exit 1
-rustup update
-rustup component add rls-preview rust-analysis rust-src rustfmt-preview
+if hash rustup &>/dev/null; then
+    rustup update
+else
+    curl https://sh.rustup.rs -sSf | sh -s -- --no-modify-path --profile complete --default-toolchain nightly -y || exit 1
+fi
 
+# rustup component add rls-preview rust-analysis rust-src rustfmt-preview
 mkdir -p ~/.zsh_completions
 rustup completions zsh       > ~/.zsh_completions/_rustup
 rustup completions zsh cargo > ~/.zsh_completions/_cargo
