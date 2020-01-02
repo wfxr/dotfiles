0fd2fc5fc0ab43d604af62b21689d515a6b611c1
Wenxuan Zhang <wenxuangm@gmail.com>
2020-01-02 09:43:34 +0800

    Python: Add pipenv installation
    Former-commit-id: 346b636cd8d1f4567f48dfe53f10b4c02b96ba7b

--- python/setup.sh
+++ python/setup.sh
@@ -2,10 +2,18 @@
 
 SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
-# Formatter / Repl
-pip install yapf ptpython
+# Formatter / Repl / Linter
+pip install yapf ptpython pylint
 
 # Config for ptpython
 mkdir -p ~/.ptpython
 ln -sf "$SCRIPT_DIR/ptpythonrc" ~/.ptpython/config.py
 ln -sf "$SCRIPT_DIR/style.yapf" ~/.style.yapf
+
+if hash apt 2>/dev/null; then
+    sudo apt update && sudo apt install pipenv -y
+elif hash pacman 2>/dev/null; then
+    sudo pacman --noconfirm -Sy python-pipenv
+elif hash brew 2>/dev/null; then
+    sudo brew install pipenv
+fi
