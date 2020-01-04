71c44df49ada87c28f894c2c22efde220d59b7d4
Wenxuan Zhang <wenxuangm@gmail.com>
2020-01-05 01:05:24 +0800

    Cleanup
    Former-commit-id: ed647242a93950257c8056c950a5726fa5342e2d

--- http/setup.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-#===============================================================================
-#   Author: Wenxuan
-#    Email: wenxuangm@gmail.com
-#  Created: 2018-03-26 11:07
-#===============================================================================
-
-sudo -H pip install --upgrade httpie
-sudo -H pip install --upgrade http-prompt
