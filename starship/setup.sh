2246b69fc4bdd2d412d9eef98bf10845b9458287
Wenxuan <wenxuangm@gmail.com>
2020-01-06 21:07:31 +0800

    Zsh: Minor fix
    Former-commit-id: 5839c6fe5159709f67ca1b314cb2bef49befcfb5

--- starship/setup.sh
+++ starship/setup.sh
@@ -12,7 +12,7 @@ SDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd) && cd "$SDIR" || exit 1
 mkdir -p ~/.config
 ln -sf "$SDIR/starship.toml" ~/.config/
 
-hash starship &>/dev/null && loginfo "starship already installed." && exit 0
+starship --version &>/dev/null && loginfo "starship already installed." && exit 0
 
 mkdir -p ~/bin
 curl -fsSL https://starship.rs/install.sh | bash -s -- -y -b ~/bin
