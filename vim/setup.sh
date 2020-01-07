800d7946f913f15bbab8017d734a764d18c9cab2
Wenxuan Zhang <wenxuangm@gmail.com>
2020-01-07 19:36:19 +0800

    Vim: Improve setup script
    Former-commit-id: 8e7f2b9b679f1508d1a8589aba0fea4a339af7a1

--- vim/setup.sh
+++ vim/setup.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 SDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd) && cd "$SDIR" || return 1
-set -euo pipefail
+set -eo pipefail
 
 loginfo()  { printf "%b[info]%b %s\n"  '\e[0;32m\033[1m' '\e[0m' "$@" >&2; }
 logwarn()  { printf "%b[warn]%b %s\n"  '\e[0;33m\033[1m' '\e[0m' "$@" >&2; }
 logerror() { printf "%b[error]%b %s\n" '\e[0;31m\033[1m' '\e[0m' "$@" >&2; }
 
 # https://github.com/neovim/neovim/wiki/Installing-Neovim
-install_nvim() {
+install_neovim() {
+    hash nvim &>/dev/null && return
+    install_nvim
     if hash apt 2>/dev/null; then
         if ! (sudo apt update && sudo apt install neovim -y); then
             loginfo "failed installing neovim from package manager. try nvim.appimage instead..."
@@ -24,47 +26,72 @@ install_nvim() {
     fi
 }
 
-setup_config() {
+install_python_support() {
+    if hash pip &>/dev/null; then
+        pip install --upgrade pynvim
+        # For tags
+        pip install --upgrade pygments
+    else
+        logwarn "failed: pip not found."
+    fi
+}
+
+install_ruby_support() {
+    if hash gem &>/dev/null; then
+        gem install neovim
+    else
+        logwarn "failed: gem not found."
+    fi
+}
+
+install_node_support() {
+    if hash npm &>/dev/null; then
+        npm install -g neovim
+    else
+        logwarn "failed: npm not found."
+    fi
+}
+
+install_configs() {
     mkdir -p ~/.config ~/.vim/spell
 
+    ln -snf "$HOME/.vim"               ~/.config/nvim
+    ln -sf  "$SDIR/vimrc"              ~/.config/nvim/init.vim
     ln -sf  "$SDIR/vimrc"              ~/.vimrc
     ln -sf  "$SDIR/gvimrc"             ~/.gvimrc
     ln -sf  "$SDIR/vim_map"            ~/.vim_map
     ln -sf  "$SDIR/vim_base"           ~/.vim_base
     ln -sf  "$SDIR/vim_plug"           ~/.vim_plug
     ln -sf  "$SDIR/spell/en.utf-8.add" ~/.vim/spell/en.utf-8.add
-    ln -sf  "$SDIR/coc-settings.json"  ~/.vim/coc-settings.json
-    ln -sf  "$HOME/.vimrc"             ~/.config/nvim/init.vim
     ln -snf "$SDIR/UltiSnips"          ~/.vim/UltiSnips
-    ln -snf "$HOME/.vim"               ~/.config/nvim
+    ln -sf  "$SDIR/coc-settings.json"  ~/.vim/coc-settings.json
     touch ~/.vim_local
 }
 
-hash nvim &>/dev/null || {
-    loginfo "neovim not installed. install it now..."
-    install_nvim
+install_plugins() {
+    if ! hash node &>/dev/null; then
+        loginfo "install node(coc.nvim need it)..."
+        mkdir ~/local
+        curl -fsSL https://install-node.now.sh/latest | sh -s -- --prefix="$HOME/.local"
+    fi
+    loginfo "install vim plugins..."
+    nvim +PlugInstall +qall
 }
 
-if hash pyenv &>/dev/null; then
-    loginfo "install python dependency..."
-    pip install --upgrade pynvim
-    # For tags
-    pip install --upgrade pygments
-else
-    logwarn "failed: pyenv not installed."
-fi
+loginfo "install neovim..."
+install_neovim
 
-if hash rbenv &>/dev/null; then
-    loginfo "install ruby dependency..."
-    gem install neovim
-fi
+loginfo "install python support..."
+install_python_support
 
-if hash nodenv &>/dev/null; then
-    loginfo "install node dependency..."
-    npm install -g neovim
-else
-    logwarn 'maybe node not installed(coc.nvim need it).'
-fi
+loginfo "install ruby support..."
+[ "$DEV_RUBY" ] && install_ruby_support
 
-loginfo "install vim plugins..."
-nvim +PlugInstall +qall
+loginfo "install node support..."
+[ "$DEV_NODE" ] && install_node_support
+
+loginfo "install configs..."
+install_configs
+
+loginfo "install plugins..."
+install_plugins
