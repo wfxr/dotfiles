4582b888c8808e490ccb3ec4a26a6e9c551aac66
Wenxuan <wenxuangm@gmail.com>
2020-01-07 14:32:19 +0800

    Node: Add node setup script
    Former-commit-id: 218e168062dfefdff991f60cad21dbaac9b9f27a

---
+++ node/setup.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+################################################################################
+#    Author: Wenxuan                                                           #
+#     Email: wenxuangm@gmail.com                                               #
+#   Created: 2020-01-07 14:00                                                  #
+################################################################################
+SDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd) && cd "$SDIR" || exit 1
+
+loginfo()  { printf "%b[info]%b %s\n"  '\e[0;32m\033[1m' '\e[0m' "$@" >&2; }
+logwarn()  { printf "%b[warn]%b %s\n"  '\e[0;33m\033[1m' '\e[0m' "$@" >&2; }
+logerror() { printf "%b[error]%b %s\n" '\e[0;31m\033[1m' '\e[0m' "$@" >&2; }
+
+export PATH="$HOME/.nodenv/bin:$PATH"
+
+install_nodenv() {
+    if ! hash nodenv &>/dev/null; then
+        git clone --depth=1 https://github.com/nodenv/nodenv.git ~/.nodenv || return 1
+        # node-build plugin
+        mkdir -p "$(nodenv root)"/plugins
+        git clone --depth=1 https://github.com/nodenv/node-build.git "$(nodenv root)/plugins/node-build"
+
+        # optional: compile dynamic bash extension to speed up nodenv
+        cd ~/.nodenv && src/configure && make -C src
+    fi
+}
+
+install_node() {
+    v=13.5.0
+    nodenv versions | grep $v || nodenv install $v
+    nodenv global $v
+}
+
+loginfo "install nodenv..."
+install_nodenv || exit $?
+
+loginfo "install node..."
+install_node || exit $?
