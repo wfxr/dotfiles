6337f2b619fca203e29f9b6b2a9033cc1ee8183c
Wenxuan Zhang <wenxuangm@gmail.com>
2020-01-16 10:33:36 +0800

    Bat: Use default color scheme
    Former-commit-id: 5ecaafefedda3b0f01a401182bf9716ee7c1efe0

--- cli/bat/setup.sh
+++ cli/bat/setup.sh
@@ -10,8 +10,8 @@ mkdir -p ~/.config/bat
 ln -sf "$SCRIPT_DIR/config" ~/.config/bat/config
 
 # Themes
-mkdir -p "$(bat --config-dir)/themes"
-cd "$(bat --config-dir)/themes" || exit 1
-test ! -d gruvbox &&
-    git clone --depth=1 --branch 3.0.0 https://github.com/Briles/gruvbox.git
-bat cache --build
+# mkdir -p "$(bat --config-dir)/themes"
+# cd "$(bat --config-dir)/themes" || exit 1
+# test ! -d gruvbox &&
+    # git clone --depth=1 --branch 3.0.0 https://github.com/Briles/gruvbox.git
+# bat cache --build
