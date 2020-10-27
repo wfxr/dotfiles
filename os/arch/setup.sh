3f50edd907e764b890d3b44f38079508789eaadf
Wenxuan Zhang <wenxuangm@gmail.com>
2020-10-27 10:34:06 +0800

    Arch: update packages

--- os/arch/setup.sh
+++ os/arch/setup.sh
@@ -8,6 +8,7 @@ cp "$SCRIPT_DIR/mirrorlist"  /etc/pacman.d/mirrorlist
 mv /etc/pacman.conf          /etc/pacman.conf.bak
 cp "$SCRIPT_DIR/pacman.conf" /etc/pacman.conf
 pacman --noconfirm -Sy archlinuxcn-keyring yay
+pacman --noconfirm -S pkgtools pkgfile && pkgfile --update
 
 #ln -sf "$SCRIPT_DIR/yaourtrc" ~/.yaourtrc
 
@@ -20,34 +21,27 @@ pacman --noconfirm -S man-pages
 pacman --noconfirm -S git-crypt
 pacman --noconfirm -S neovim
 pacman --noconfirm -S openssh make cmake
-pacman --noconfirm -S clang go
+# pacman --noconfirm -S clang
+pacman --noconfirm -S go
 pacman --noconfirm -S zsh htop
 pacman --noconfirm -S words
-pacman --noconfirm -S perl-rename
 # pacman --noconfirm -S jdk9-openjdk openjdk9-src
 # pacman --noconfirm -S pet-git
 # pacman --noconfirm -S mosh
 pacman --noconfirm -S axel
 pacman --noconfirm -S shellcheck
 pacman --noconfirm -S highlight
-pacman --noconfirm -S jq pv pigz
-pacman --noconfirm -S nethogs
-yay    --noconfirm -S icdiff # better diff
+pacman --noconfirm -S jq pv
+# pacman --noconfirm -S nethogs
 # pacman --noconfirm -S global # gtags
 pacman --noconfirm -S universal-ctags-git # better ctags
 pacman --noconfirm -S ncdu   # NCurses Disk Usage
 # pacman --noconfirm -S create_ap # Create AP
-pacman --noconfirm -S fd exa bat ripgrep vivid sd
-
-pacman --noconfirm -S lightscreen # light weight screenshot tool
-
-# pacman --noconfirm -S shadowsocks-libev ipset
-# systemctl daemon-reload
-# systemctl enable ipset
-# systemctl start  ipset
-
-
-pacman --noconfirm -S xclip shutter
+pacman --noconfirm -S fd exa bat ripgrep sd
+pacman --noconfirm -S vivid
+pacman --noconfirm -S xclip
+# pacman --noconfirm -S lightscreen # light weight screenshot tool
+# pacman --noconfirm -S shutter
 
 # crontab
 pacman --noconfirm -S cronie
@@ -61,7 +55,7 @@ pacman --noconfirm -S fcitx fcitx-gtk2 fcitx-gtk3 fcitx-qt4 fcitx-qt5 fcitx-conf
 ln -sf "$SCRIPT_DIR/xprofile" ~/.xprofile
 
 # cool-retro-term is a terminal emulator which mimics the look and feel of the old cathode tube screens.
-pacman --noconfirm -S cool-retro-term
+# pacman --noconfirm -S cool-retro-term
 pacman --noconfirm -S xdotool
 
 pacman --noconfirm -S screenkey
@@ -71,8 +65,6 @@ pacman --noconfirm -S pacman-contrib
 systemctl daemon-reload
 systemctl enable paccache.timer
 systemctl start  paccache.timer
-
-#pacman --noconfirm -S btrfs-progs snapper
 
 # A very simple screen locker for X
 # https://tools.suckless.org/slock/
@@ -95,3 +87,9 @@ TOKEN
 systemctl daemon-reload
 systemctl start slock@wenxuan.service
 systemctl enable slock@wenxuan.service
+
+# yay --noconfirm -S icdiff # better diff
+yay --noconfirm -S mmv-go
+yay --noconfirm -S csview-git
+yay --noconfirm -S code-minimap-git
+yay --noconfirm -S git-delta-git
