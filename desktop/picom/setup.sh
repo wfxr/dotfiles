3eaa28e84b6be6004cfbd42b5c5872a5666f2884
Wenxuan Zhang <wenxuangm@gmail.com>
2020-10-28 15:39:43 +0800

    Picom: add picom config

---
+++ desktop/picom/setup.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+############################################################################
+#    Author: Wenxuan Zhang                                                 #
+#     Email: wenxuangm@gmail.com                                           #
+#   Created: 2020-10-28 14:35                                              #
+############################################################################
+set -euo pipefail
+IFS=$'\n\t'
+
+SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd) && cd "$SCRIPT_DIR" || return 1
+
+mkdir -p ~/.config/picom
+ln -sf "$SCRIPT_DIR/picom.conf" ~/.config/picom/picom.conf
