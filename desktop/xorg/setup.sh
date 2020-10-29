48652370a9bef9dd85b57511bddda7b5104af142
Wenxuan Zhang <wenxuangm@gmail.com>
2020-10-29 19:12:28 +0800

    Xorg: add xthemes

--- desktop/xorg/setup.sh
+++ desktop/xorg/setup.sh
@@ -6,3 +6,4 @@ ln -sf "$SDIR/xprofile"        ~/.xprofile
 ln -sf "$SDIR/xinitrc"         ~/.xinitrc
 ln -sf "$SDIR/Xresources"      ~/.Xresources
 ln -sf "$SDIR/pam_environment" ~/.pam_environment
+ln -sf "$SDIR/xthemes"         ~/.xthemes
