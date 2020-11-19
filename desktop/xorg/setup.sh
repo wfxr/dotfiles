d10c98457d50435f9b7801d56509ba027baee7be
Wenxuan Zhang <wenxuangm@gmail.com>
2020-11-19 11:41:02 +0800

    Xorg: fix themes

--- desktop/xorg/setup.sh
+++ desktop/xorg/setup.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
+set -euo pipefail
+IFS=$'\n\t'
 
-SDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" && cd "$SDIR" || return 1
+SDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" && cd "$SDIR"
 
-ln -sf "$SDIR/xprofile"        ~/.xprofile
-ln -sf "$SDIR/xinitrc"         ~/.xinitrc
-ln -sf "$SDIR/Xresources"      ~/.Xresources
-ln -sf "$SDIR/pam_environment" ~/.pam_environment
-ln -sf "$SDIR/xthemes"         ~/.xthemes
+ln -sf  "$SDIR/xprofile"        ~/.xprofile
+ln -sf  "$SDIR/xinitrc"         ~/.xinitrc
+ln -sf  "$SDIR/Xresources"      ~/.Xresources
+ln -snf "$SDIR/xthemes"         ~/.xthemes
