6266e556b62cb7332a8c1c6999d2e2c65607a653
Wenxuan Zhang <wenxuangm@gmail.com>
2021-03-31 17:20:42 +0800

    Font: Sarasa

--- os/arch/fonts.sh
+++ os/arch/fonts.sh
@@ -3,6 +3,8 @@
 
 SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd) && cd "$SCRIPT_DIR" || exit 1
 
-pacman --noconfirm -S noto-fonts noto-fonts-cjk noto-fonts-emoji
-pacman --noconfirm -S nerd-fonts-complete
-yay -S ttf-meslo
+paru --noconfirm -S noto-fonts noto-fonts-cjk noto-fonts-emoji
+paru --noconfirm -S nerd-fonts-complete
+paru --noconfirm -S ttf-meslo
+paru --noconfirm -S ttf-iosevka-term
+paru --noconfirm -S ttf-sarasa-gothic
