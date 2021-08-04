8926378191bc78003008967093cf61fb323be623
Wenxuan Zhang <wenxuangm@gmail.com>
2021-08-04 11:10:41 +0800

    Scala: add scala scripts

---
+++ sdk/scala/setup.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -euo pipefail
+IFS=$'\n\t'
+
+SDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd) && cd "$SDIR" || exit 1
+
+mkdir -p ~/bin
+ln -sf "$SDIR/sbtnew" ~/bin/sbtnew
