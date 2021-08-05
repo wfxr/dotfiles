a1a5d4bdd30fe18bf0e7e69afa3c668b02999bb2
Wenxuan Zhang <wenxuangm@gmail.com>
2021-08-05 10:06:39 +0800

    Scala: misc

--- sdk/scala/setup.sh
+++ sdk/scala/setup.sh
@@ -4,5 +4,18 @@ IFS=$'\n\t'
 
 SDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd) && cd "$SDIR" || exit 1
 
+# Install coursier
+if ! hash cs &>/dev/null; then
+    mkdir ~/bin
+    curl -fLo ~/bin/cs https://git.io/coursier-cli-"$(uname | tr LD ld)"
+    chmod +x ~/bin/cs
+fi
+
+# Install tools from coursier
+for cmd in scalafmt scalafix; do
+    hash $cmd &>/dev/null || cs install $cmd
+done
+
+# Install util scripts
 mkdir -p ~/bin
 ln -sf "$SDIR/sbtnew" ~/bin/sbtnew
