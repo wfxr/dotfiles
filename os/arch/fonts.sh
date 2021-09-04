d1ce7f444c4b99e79cbe9f8c6b192addd23a4c9a
Wenxuan Zhang <wenxuangm@gmail.com>
2021-09-04 18:04:39 +0800

    Arch: fix script

--- os/arch/fonts.sh
+++ os/arch/fonts.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
-[ "$(whoami)" != "root" ] && exec sudo -- "$0" "$@"
-
-SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd) && cd "$SCRIPT_DIR" || exit 1
 
 paru --noconfirm -S noto-fonts noto-fonts-cjk noto-fonts-emoji
 # paru --noconfirm -S nerd-fonts-complete
