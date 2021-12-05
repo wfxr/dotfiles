70c82b576c846907c1b2833713482a2881edc547
Wenxuan Zhang <wenxuangm@gmail.com>
2021-12-05 18:17:56 +0800

    Fonts: clean up

--- fonts/emoji.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-pacman -S noto-fonts-emoji
--- fonts/nerd-fonts.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-directory=.ghq/github.com/ryanoasis/
-mkdir -p $directory && cd $directory || exit 1
-git clone --depth 1 https://github.com/ryanoasis/nerd-fonts || cd nerd-fonts || exit 1
-git pull
-nerd-fonts/install.sh
