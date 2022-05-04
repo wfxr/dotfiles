bc91410c5a4f39f8b2f1acc9423aa838e812670c
Wenxuan Zhang <wenxuangm@gmail.com>
2022-05-04 20:04:23 +0800

    Vim: update indent line config

--- vim/nvim/lua/modules/ui/config.lua
+++ vim/nvim/lua/modules/ui/config.lua
@@ -404,32 +404,26 @@ function config.gitsigns()
 end
 
 function config.indent_blankline()
-    vim.opt.termguicolors = true
-    vim.opt.list = true
     require("indent_blankline").setup({
         char = "│",
-        filetype_exclude = {
-            "startify",
-            "dashboard",
-            "dotooagenda",
-            "log",
-            "fugitive",
-            "gitcommit",
-            "packer",
-            "vimwiki",
-            "markdown",
-            "json",
-            "txt",
-            "vista",
-            "help",
-            "todoist",
-            "NvimTree",
-            "peekaboo",
-            "git",
-            "TelescopePrompt",
-            "undotree",
-            "flutterToolsOutline",
-            "", -- for all buffers without a file type
+        filetype = {
+            "c",
+            "cpp",
+            "go",
+            "lua",
+            "java",
+            "kotlin",
+            "scala",
+            "clojure",
+            "rust",
+            "sh",
+            "zsh",
+            "vim",
+            "ruby",
+            "python",
+            "make",
+            "toml",
+            "yaml",
         },
         buftype_exclude = { "terminal", "nofile" },
         show_trailing_blankline_indent = false,
