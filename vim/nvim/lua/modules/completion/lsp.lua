999dde277259728839527965b2799bdf486d6598
Wenxuan Zhang <wenxuangm@gmail.com>
2022-05-15 19:29:12 +0800

    Vim: update lsp installer config

--- vim/nvim/lua/modules/completion/lsp.lua
+++ vim/nvim/lua/modules/completion/lsp.lua
@@ -36,7 +36,14 @@ saga.init_lsp_saga({
     rename_action_keys = { quit = { '<C-[>', '<C-c>', '<Esc>' }, exec = '<CR>' },
 })
 
-lsp_installer.settings({
+lsp_installer.setup({
+    ensure_installed = {
+        "efm",
+        "gopls",
+        "rust_analyzer",
+        "bashls",
+        "sumneko_lua",
+    },
     ui = {
         icons = {
             server_installed = "✓",
@@ -44,9 +51,23 @@ lsp_installer.settings({
             server_uninstalled = "✗",
         },
     },
+    keymaps = {
+        -- Keymap to expand a server in the UI
+        toggle_server_expand = "o",
+        -- Keymap to install the server under the current cursor position
+        install_server = "i",
+        -- Keymap to reinstall/update the server under the current cursor position
+        update_server = "u",
+        -- Keymap to check for new version for the server under the current cursor position
+        check_server_version = "c",
+        -- Keymap to update all installed servers
+        update_all_servers = "U",
+        -- Keymap to check which installed servers are outdated
+        check_outdated_servers = "C",
+        -- Keymap to uninstall a server
+        uninstall_server = "X",
+    },
 })
-
-lsp_installer.setup({})
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
--- vim/setup.sh
+++ vim/setup.sh
@@ -12,4 +12,7 @@ ln -sf  "$SDIR/ideavimrc" ~/.ideavimrc
 mgrdir=~/.local/share/nvim/site/pack/packer/start/packer.nvim
 [ -d $mgrdir ] || git clone --depth 1 https://github.com/wbthomason/packer.nvim $mgrdir
 nvim --headless +'autocmd User PackerComplete sleep 1000m | quitall' +'PackerSync'
-nvim --headless +'LspInstall --sync efm gopls rust_analyzer bashls' +qall
+
+for lsp in efm gopls rust_analyzer bashls sumneko_lua; do
+    nvim --headless +"LspInstall --sync $lsp" +qall
+done
