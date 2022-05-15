b87323f64a785ae604e1ca96ec8cf3fe9e87b928
Wenxuan Zhang <wenxuangm@gmail.com>
2022-05-15 10:35:52 +0800

    Tmux: update setup script

--- tmux/setup.sh
+++ tmux/setup.sh
@@ -9,5 +9,5 @@ ln -sf "$SCRIPT_DIR/tmux.conf" ~/.tmux.conf
 # tpm
 [[ ! -a ~/.tmux/plugins/tpm ]] && git clone --depth=1 https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
-# tmuxinator
-ln -snf "$SCRIPT_DIR/tmuxinator" ~/.tmuxinator
+# install plugins
+~/.tmux/plugins/tpm/scripts/install_plugins.sh
