7a21dfd8b4b63b36bb96673525258e6fa2307dd4
Wenxuan Zhang <wenxuangm@gmail.com>
2022-06-20 18:25:19 +0800

    Vim: update dap-ui config

--- vim/nvim/lua/modules/editor/config.lua
+++ vim/nvim/lua/modules/editor/config.lua
@@ -145,21 +145,26 @@ function config.dapui()
             edit = "e",
             repl = "r",
         },
-        sidebar = {
-            elements = {
-                -- Provide as ID strings or tables with "id" and "size" keys
-                {
-                    id = "scopes",
-                    size = 0.25, -- Can be float or integer > 1
+        layouts = {
+            {
+                elements = {
+                    'scopes',
+                    'breakpoints',
+                    'stacks',
+                    'watches',
                 },
-                { id = "breakpoints", size = 0.25 },
-                { id = "stacks", size = 0.25 },
-                { id = "watches", size = 00.25 },
+                size = 40,
+                position = 'left',
             },
-            size = 40,
-            position = "left",
+            {
+                elements = {
+                    'repl',
+                    'console',
+                },
+                size = 10,
+                position = 'bottom',
+            },
         },
-        tray = { elements = { "repl" }, size = 10, position = "bottom" },
         floating = {
             max_height = nil,
             max_width = nil,
