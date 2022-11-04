07b56f1633f2dc39f3b62d9f67913ac1f60d103f
Wenxuan Zhang <wenxuangm@gmail.com>
2022-11-04 18:06:45 +0800

    Vim: add hlslens
    Signed-off-by: Wenxuan Zhang <wenxuangm@gmail.com>

--- vim/nvim/lua/modules/editor/plugins.lua
+++ vim/nvim/lua/modules/editor/plugins.lua
@@ -149,5 +149,19 @@ editor["sindrets/diffview.nvim"] = {
     opt = true,
     cmd = { "DiffviewOpen" },
 }
+editor['kevinhwang91/nvim-hlslens'] = {
+    config = function ()
+        require('hlslens').setup({
+            nearest_only = true,
+        })
+        local kopts = {noremap = true, silent = true}
+        vim.api.nvim_set_keymap('n', 'n', [[<Cmd>execute('normal! ' . v:count1 . 'n')<CR><Cmd>lua require('hlslens').start()<CR>]], kopts)
+        vim.api.nvim_set_keymap('n', 'N', [[<Cmd>execute('normal! ' . v:count1 . 'N')<CR><Cmd>lua require('hlslens').start()<CR>]], kopts)
+        vim.api.nvim_set_keymap('n', '*', [[*<Cmd>lua require('hlslens').start()<CR>]], kopts)
+        vim.api.nvim_set_keymap('n', '#', [[#<Cmd>lua require('hlslens').start()<CR>]], kopts)
+        vim.api.nvim_set_keymap('n', 'g*', [[g*<Cmd>lua require('hlslens').start()<CR>]], kopts)
+        vim.api.nvim_set_keymap('n', 'g#', [[g#<Cmd>lua require('hlslens').start()<CR>]], kopts)
+    end
+}
 
 return editor
