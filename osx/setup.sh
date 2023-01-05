c2f727725a16581134ed217e117e47cfae83dcf5
Wenxuan Zhang <wenxuangm@gmail.com>
2023-01-05 11:10:41 +0800

    OSX: add some packages
    Signed-off-by: Wenxuan Zhang <wenxuangm@gmail.com>

--- osx/setup.sh
+++ osx/setup.sh
@@ -4,5 +4,8 @@ IFS=$'\n\t'
 
 SDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd) && cd "$SDIR" || exit 1
 
+mkdir -p "$HOME/.config/karabiner"
+ln -sf "$SDIR/karabiner.json" "$HOME/.config/karabiner/karabiner.json"
+
 # disable mouse acceleration
 defaults write .GlobalPreferences com.apple.mouse.scaling -1
