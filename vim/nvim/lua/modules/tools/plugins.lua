498734f35f3a1bcb8d732daddb217a12fa51fcd4
Wenxuan Zhang <wenxuangm@gmail.com>
2023-02-09 10:49:36 +0800

    Vim: add git-conflict plugin
    Signed-off-by: Wenxuan Zhang <wenxuangm@gmail.com>

--- vim/nvim/lua/modules/tools/plugins.lua
+++ vim/nvim/lua/modules/tools/plugins.lua
@@ -90,8 +90,8 @@ tools["chentoast/marks.nvim"] = {
             cyclic = true,
             -- whether the shada file is updated after modifying uppercase marks. default false
             force_write_shada = false,
-            -- how often (in ms) to redraw signs/recompute mark positions. 
-            -- higher values will have better performance but may cause visual lag, 
+            -- how often (in ms) to redraw signs/recompute mark positions.
+            -- higher values will have better performance but may cause visual lag,
             -- while lower values may cause performance penalties. default 150.
             refresh_interval = 250,
             -- sign priorities for each type of mark - builtin marks, uppercase marks, lowercase
@@ -99,7 +99,7 @@ tools["chentoast/marks.nvim"] = {
             -- can be either a table with all/none of the keys, or a single number, in which case
             -- the priority applies to all marks.
             -- default 10.
-            sign_priority = { lower=10, upper=15, builtin=8, bookmark=20 },
+            sign_priority = { lower = 10, upper = 15, builtin = 8, bookmark = 20 },
             -- disables mark tracking for specific filetypes. default {}
             excluded_filetypes = {},
             mappings = {}
@@ -110,10 +110,30 @@ tools["chentoast/marks.nvim"] = {
 tools["ojroques/nvim-osc52"] = {
     opt = false,
     config = function()
-        vim.keymap.set('n', '<leader>y', require('osc52').copy_operator, {expr = true})
-        vim.keymap.set('n', 'c-y', '<leader>c_', {remap = true})
+        vim.keymap.set('n', '<leader>y', require('osc52').copy_operator, { expr = true })
+        vim.keymap.set('n', 'c-y', '<leader>c_', { remap = true })
         vim.keymap.set('x', 'c-y', require('osc52').copy_visual)
     end
 }
+
+tools["akinsho/git-conflict.nvim"] = {
+    config = function()
+        require('git-conflict').setup()
+    end
+}
+
+-- tools["williamboman/mason.nvim"] = {
+--     opt = false,
+--     config = function()
+--         require("mason").setup({
+--             ui = {
+--                 icons = {
+--                     package_installed = "",
+--                     package_pending = "➜",
+--                 }
+--             }
+--         })
+--     end
+-- }
 
 return tools
