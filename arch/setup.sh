c14141cc4e1e382dfa15ecbccf3ae7b10d6d96f2
Wenxuan Zhang <wenxuangm@gmail.com>
2023-06-13 11:58:18 +0800

    Arch: update common dir name
    Signed-off-by: Wenxuan Zhang <wenxuangm@gmail.com>

--- arch/setup.sh
+++ arch/setup.sh
@@ -7,13 +7,7 @@ SDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd) && cd "$SDIR" || exit 1
 mkdir -p "$HOME/.config"
 mkdir -p "$HOME/tmp"
 mkdir -p "$HOME/bin"
-mkdir -p "$HOME/downloads"
-mkdir -p "$HOME/documents"
-mkdir -p "$HOME/music"
-mkdir -p "$HOME/pictures"
-mkdir -p "$HOME/videos"
-mkdir -p "$HOME/screenshots"
-mkdir -p "$HOME/develop"
+mkdir -p "$HOME/dev"
 
 mkdir -p ~/.config/paru
 ln -sf "$SDIR/paru.conf" ~/.config/paru/paru.conf
