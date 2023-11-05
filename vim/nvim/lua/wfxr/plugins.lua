888c959c8baad1f217e2b26a6566a284807dd9c1
Wenxuan Zhang <wenxuangm@gmail.com>
2023-11-05 23:18:17 +0800

    Vim: add flash.nvim
    Signed-off-by: Wenxuan Zhang <wenxuangm@gmail.com>

--- vim/nvim/lua/wfxr/plugins.lua
+++ vim/nvim/lua/wfxr/plugins.lua
@@ -44,7 +44,17 @@ local plugins = {
 
     -- things that either enhance builtin behaviours or could easily be candidates for default behaviour
     {
-        "luukvbaal/statuscol.nvim",
+        "folke/flash.nvim",
+        {
+            "luukvbaal/statuscol.nvim",
+            event = "VeryLazy",
+            keys = {
+                { "s", mode = { "n", "x", "o" }, function() require("flash").jump()              end, desc = "Flash"             },
+                { "+", mode = { "n", "x", "o" }, function() require("flash").treesitter()        end, desc = "Flash Treesitter"  },
+                { "r", mode = { "o"           }, function() require("flash").remote()            end, desc = "Remote Flash"      },
+                { "R", mode = { "o", "x"      }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+            },
+        },
         "airblade/vim-rooter",
         "ggandor/leap.nvim",
         "lewis6991/hover.nvim",
