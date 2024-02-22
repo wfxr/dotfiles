8b7222bb86a1f77977106787823ce6246269090e
Wenxuan Zhang <wenxuangm@gmail.com>
2024-02-22 11:52:43 +0800

    Rust: rename cargo config
    Signed-off-by: Wenxuan Zhang <wenxuangm@gmail.com>

--- sdk/rust/setup.sh
+++ sdk/rust/setup.sh
@@ -4,7 +4,7 @@
 SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
 mkdir -p ~/.cargo
-ln -snf "$SCRIPT_DIR/config"       ~/.cargo/config
+ln -snf "$SCRIPT_DIR/config.toml"  ~/.cargo/config.toml
 ln -snf "$SCRIPT_DIR/rustfmt.toml" ~/.rustfmt.toml
 
 if hash rustup &>/dev/null; then
