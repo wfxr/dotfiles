a62ab5dc47d3919e97bd3aeaf2fbf4547138f728
Wenxuan Zhang <wenxuangm@gmail.com>
2024-06-26 11:52:08 +0800

    Vim: add nvim-config-local plugin
    Signed-off-by: Wenxuan Zhang <wenxuangm@gmail.com>

--- vim/nvim/lua/plugins/editor.lua
+++ vim/nvim/lua/plugins/editor.lua
@@ -127,4 +127,22 @@ return {
       vim.cmd([[do FileType]])
     end,
   },
+
+  {
+    "klen/nvim-config-local",
+    config = function()
+      require("config-local").setup({
+        -- Config file patterns to load (lua supported)
+        config_files = { ".nvim.lua", ".nvimrc", ".exrc" },
+
+        -- Where the plugin keeps files data
+        hashfile = vim.fn.stdpath("data") .. "/config-local",
+
+        autocommands_create = true, -- Create autocommands (VimEnter, DirectoryChanged)
+        commands_create = true, -- Create commands (ConfigLocalSource, ConfigLocalEdit, ConfigLocalTrust, ConfigLocalIgnore)
+        silent = false, -- Disable plugin messages (Config loaded/ignored)
+        lookup_parents = false, -- Lookup config files in parent directories
+      })
+    end,
+  },
 }
