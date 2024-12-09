2d4a4c9210ada7904c19664b0599e5cc5452bf3f
Wenxuan Zhang <wenxuangm@gmail.com>
2024-12-09 16:59:30 +0800

    Yazi: init config

---
+++ yazi/setup.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -euo pipefail
+IFS=$'\n\t'
+
+SDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd) && cd "$SDIR" || return 1
+
+info() { printf "%b[info]%b %s\n" '\e[0;32m\033[1m' '\e[0m' "$*" >&2; }
+warn() { printf "%b[warn]%b %s\n" '\e[0;33m\033[1m' '\e[0m' "$*" >&2; }
+erro() { printf "%b[erro]%b %s\n" '\e[0;31m\033[1m' '\e[0m' "$*" >&2; }
+
+mkdir -p ~/.config/yazi
+
+ln -sf "$SDIR/yazi.toml"   ~/.config/yazi/
+ln -sf "$SDIR/keymap.toml" ~/.config/yazi/
+ln -sf "$SDIR/theme.toml"  ~/.config/yazi/
